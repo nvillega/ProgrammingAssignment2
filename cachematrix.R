fd64317005d03eb1bcb48f189ce6a76112701ef2
Norha M. Villegas <nvillega@icesi.edu.co>
2016-02-10 23:54:48 -0500

    solution to assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions implement a caching mechanism for the calculation of the inverse of a matrix
 
-## Write a short comment describing this function
+## This function implements gets and sets functions for a matrix object and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+
+## This function returns a matrix that is the inverse of 'x' using the caching mechanism implemented
+## in the above function (the cache is sent as arg in this function). First it checks whether the inverse of the matrix already exists (getinverse).
+## If it exists return the obtained valu thus avoiding the calculation. If the getInverse() returns a null 
+## value, then the function calculates the inverse, stores the result using the setinverse function and 
+## returns the inverse of the original matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
+
+
+####To test this submission, source the functions above and run the following code
+
+# Creating a squared matrix
+seq1 <- seq(1:4)
+mat1 <- matrix(seq1, 2)
+mat1
+#Calculating the inverse without the caching mechanism
+solve(mat1)
+
+#Checking get functions before calling the cacheSolve function
+cache <- makeCacheMatrix(mat1)
+#to get the matrix
+cache$get()
+#to get the cached inverse (null at this time)
+cache$getinverse()
+
+#Invoking the cacheSolve method to obtain the inverse using the caching mechanism
+inversemat1 <- cacheSolve(cache)
+inversemat1
